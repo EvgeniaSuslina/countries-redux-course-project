effe71a27b3ebb0ae72d7f80174555f138677608
Evgenia <91392528+EvgeniaSuslina@users.noreply.github.com>
2023-02-08 00:31:35 +0300

    first commit

---
+++ src/store/countries/countries-reducers.js
@@ -0,0 +1,33 @@
+import { SET_COUNTRIES, SET_LOADING, SET_ERROR } from "./countries-actions"
+
+const initialState = {
+    status: 'idle', // loading | recieved | rejected
+    error: null,
+    list: [],
+}
+
+export const countriesReducer = (state = initialState, {type, payload}) => {
+    switch(type) {
+        case SET_COUNTRIES: 
+      return {
+        ...state,
+        status: 'recieved',
+        list: payload,        
+      };
+
+      case SET_LOADING: 
+      return {
+        ...state,
+        status: 'loading',
+        error: null
+      }
+      case SET_ERROR :
+        return {
+          ...state,
+          status: 'rejected',
+          error: payload
+        }
+        default: 
+        return state;
+    }
+}
